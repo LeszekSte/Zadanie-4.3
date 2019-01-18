91d73d1867e0cc5d6761469df4d9b9e9b0d44c98
LeszekSte <34620437+LeszekSte@users.noreply.github.com>
2019-01-18 18:15:46 +0100

    Update Calculations.java
    Już działa ok (Poprawiona funkcja pow)

--- src/Calculations.java
+++ src/Calculations.java
@@ -2,9 +2,9 @@ public class Calculations {
 
     public boolean isRightTriangel(Triangle triangle) {
 
-        double a = Math.pow(2, triangle.getBokA());
-        double b = Math.pow(2, triangle.getBokB());
-        double c = Math.pow(2, triangle.getBokC());
+        double a = Math.pow(triangle.getBokA(),2);
+        double b = Math.pow(triangle.getBokB(),2);
+        double c = Math.pow(triangle.getBokC()),2;
         boolean isRightTriangel  = false;
 
 
